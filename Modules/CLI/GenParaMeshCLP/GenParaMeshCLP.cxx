a80c3e1411fbfa53195cd640682f9627ad0372cd
Zhengyang Fang <tonyfang2018@outlook.com>
2018-03-09 00:14:45 -0500

    Add Modularity and unique namePrefix for generated files

--- Modules/CLI/GenParaMeshCLP/GenParaMeshCLP.cxx
+++ Modules/CLI/GenParaMeshCLP/GenParaMeshCLP.cxx
@@ -8,6 +8,12 @@
 #include <fstream>
 #include <string>
 #include <string.h>
+//stringstream
+#include <sstream>
+// std::cout
+#include <iostream>
+// std::vector
+#include <vector>
 
 #include <itkImage.h>
 #include <itkImageFileReader.h>
@@ -35,10 +41,33 @@
 #include "itkVTKPolyDataWriter.h"
 #include "myConformalFlatteningMeshFilter.h"
 
+typedef itk::Image<unsigned short, 3>                             ImageType;
+typedef itk::ImageFileReader<ImageType>                           VolumeReaderType;
+typedef itk::BinaryMask3DEqualAreaParametricMeshSource<ImageType> MeshSourceType;
+typedef MeshSourceType::OutputMeshType                            OutputMeshType;
+typedef itk::MeshSpatialObject<OutputMeshType>                    MeshSpatialObjectType;
+typedef itk::ImageFileReader<ImageType> VolumeReaderType;
+
+typedef itk::DefaultDynamicMeshTraits<double, 3, 3, double, double> MeshTraitsType;
+typedef itk::Mesh<double, 3, MeshTraitsType>                        itkMeshType;
+typedef itk::MeshSpatialObject<itkMeshType>                         itkMeshSOType;
+typedef itk::MetaMeshConverter<3, double, MeshTraitsType>           MeshConverterType;
+
+// ConformalMapping
+// For future work, make "typedef itk::Mesh< double, 3, MeshTraitsType > MeshType;" work, need some revision
+// on myConformalFlatteningFilter.txx
+
+typedef itk::Mesh< double, 3 > MeshType;
+typedef itk::ConformalFlatteningMeshFilter<MeshType, MeshType>  FilterType;
+typedef MeshType::CellIdentifier  CellIdentifier;
+typedef itk::VTKPolyDataReader<MeshType>  ITKReaderType;
+typedef itk::VTKPolyDataWriter<MeshType>  ITKWriterType;
 
 using namespace std;
 void WriteEulerFile( std::string outEulerName, int Eulernum);
 vtkSmartPointer<vtkPolyData> ReadPolyData(std::string filePath);
+void WritePolyData(MeshType::Pointer meshInput, std::string& fileName);
+std::string generatePrefix(std::string ConfOrInit, std::string infile, std::string initParaFileName, int numIterations);
 
 // static int debug = 0;
 
@@ -46,28 +75,7 @@ int main( int argc, char * argv[] )
 {
 
   PARSE_ARGS;
-  typedef itk::Image<unsigned short, 3>                             ImageType;
-  typedef itk::ImageFileReader<ImageType>                           VolumeReaderType;
-  typedef itk::BinaryMask3DEqualAreaParametricMeshSource<ImageType> MeshSourceType;
-  typedef MeshSourceType::OutputMeshType                            OutputMeshType;
-  typedef itk::MeshSpatialObject<OutputMeshType>                    MeshSpatialObjectType;
-  typedef itk::ImageFileReader<ImageType> VolumeReaderType;
-
-  typedef itk::DefaultDynamicMeshTraits<double, 3, 3, double, double> MeshTraitsType;
-  typedef itk::Mesh<double, 3, MeshTraitsType>                        itkMeshType;
-  typedef itk::MeshSpatialObject<itkMeshType>                         itkMeshSOType;
-  typedef itk::MetaMeshConverter<3, double, MeshTraitsType>           MeshConverterType;
-
-  MeshSourceType::Pointer meshsrc = MeshSourceType::New();
-
-  // ConformalMapping
-  typedef itk::Mesh< double, 3 > MeshType;
-  typedef itk::ConformalFlatteningMeshFilter<MeshType, MeshType>  FilterType;
-  typedef MeshType::CellIdentifier  CellIdentifier;
-  typedef itk::VTKPolyDataReader<MeshType>  ITKReaderType;
-  typedef itk::VTKPolyDataWriter<MeshType>  ITKWriterType;
-
-
+  // Read the input image
   ImageType::Pointer image;
   bool               initParaFile;
   if( initParaFileName == "NULL" )
@@ -86,20 +94,18 @@ int main( int argc, char * argv[] )
   imageReader->SetFileName(infile);
   imageReader->Update();
   image = imageReader->GetOutput();
-  OutputMeshType::Pointer mesh;
-  OutputMeshType::Pointer mesh0;
-  OutputMeshType::Pointer parmesh;
-
+  // Create log file if log flag is on
   ofstream log;
-
   if( logFile )
     {
         log.open(outLogName.c_str(), ios::out | ios::app);
     }
 
-
   // if necessary read parmesh. When --conf is set, no initParaFile allowed(--conf set confParaMesh as default initPara); otherwise, initParaFile is allowed
-
+  MeshSourceType::Pointer meshsrc = MeshSourceType::New();
+  OutputMeshType::Pointer mesh;
+  OutputMeshType::Pointer mesh0;
+  OutputMeshType::Pointer parmesh;
   try
   {
         if( debug )
@@ -115,6 +121,7 @@ int main( int argc, char * argv[] )
             meshsrc->Update();
             // Output Mesh
             mesh0 = meshsrc->GetSurfaceMesh();
+
             // convert surfaces to VTK
             itkMeshTovtkPolyData ITKVTKConverter;
             ITKVTKConverter.SetInput(mesh0);
@@ -133,13 +140,19 @@ int main( int argc, char * argv[] )
             #else
             SurfMesh0writer->SetInput(SurfMesh0);
             #endif
-            SurfMesh0writer->SetFileName("SurfMesh0.vtk");
-            SurfMesh0writer->Write();
+            std::string Surf0Common = generatePrefix("ConfInitPara", infile, "", numIterations);
 
+            //write the SurfMesh0Name filename as Surf0Common-surf0.vtk
+            std::string SurfMesh0Name("dummy");
+            SurfMesh0Name.erase();
+            SurfMesh0Name.append(Surf0Common);
+            SurfMesh0Name.append("-surf0.vtk");
+            SurfMesh0writer->SetFileName(SurfMesh0Name.c_str());
+            SurfMesh0writer->Write();
 
             // Read the SurfMesh0 file
             ITKReaderType::Pointer ITKreader = ITKReaderType::New();
-            ITKreader->SetFileName("SurfMesh0.vtk");
+            ITKreader->SetFileName(SurfMesh0Name.c_str());
             try {
                 ITKreader->Update();
             }
@@ -148,6 +161,13 @@ int main( int argc, char * argv[] )
                 return EXIT_FAILURE;
             }
             MeshType::Pointer SurfMesh0_R = ITKreader->GetOutput();
+
+
+            //try to make this work in the future. MeshType can't be initialized to have the same mesh
+            //traits as the BinaryMask3DEqualAreaParametricMeshSource because conformal filter
+            //does not work with that mesh traits
+            //MeshType::Pointer SurfMesh0_R = mesh0;
+
             // Use the output mesh to run ConformalMapping
             FilterType::Pointer filter = FilterType::New();
             filter->SetInput(SurfMesh0_R);
@@ -170,35 +190,34 @@ int main( int argc, char * argv[] )
             // Get the Smart Pointer to the Filter Output
             MeshType::Pointer Confpara = filter->GetOutput();
             // Write as VTK
-            ITKWriterType::Pointer ITKwriter = ITKWriterType::New();
-            ITKwriter->SetInput(Confpara);
-            ITKwriter->SetFileName("Confpara.vtk");
-            ITKwriter->Update();
-
+            std::string ConfPara0Name("dummy");
+            ConfPara0Name.erase();
+            ConfPara0Name.append(Surf0Common);
+            ConfPara0Name.append("-confPara0.vtk");
+            //write VTK
+            WritePolyData(Confpara, ConfPara0Name);
             // Read VTK
-            vtkSmartPointer<vtkPolyDataReader> reader = vtkSmartPointer<vtkPolyDataReader>::New();
-            reader->SetFileName( "Confpara.vtk" );
-            reader->Update();
-            vtkSmartPointer<vtkPolyData> polydata = reader->GetOutput();
+            vtkSmartPointer<vtkPolyData> polydata = ReadPolyData(ConfPara0Name);
             //make sure it reads the VTK correctly
             if( debug )
             {
-                std::cout << "Confpara.vtk has " << polydata->GetNumberOfPoints() << " points." << std::endl;
+                std::cout << ConfPara0Name << " has " << polydata->GetNumberOfPoints() << " points." << std::endl;
             }
-
             // convert to itk mesh data structure
             vtkPolyDataToitkMesh vtkItkConverter;
             vtkItkConverter.SetInput( polydata );
-
             if (debug) {
-                std::cout << "converting mesh Confpara.vtk" <<std::endl;
+                std::cout << "converting mesh " << ConfPara0Name <<std::endl;
             }
-
             // write out the itk meta mesh file
             itkMeshSOType::Pointer meshSO = itkMeshSOType::New();
             meshSO->SetMesh( vtkItkConverter.GetOutput() );
             MeshConverterType::Pointer itkConverter = MeshConverterType::New();
-            itkConverter->WriteMeta( meshSO, "Confpara.meta" );
+            std::string ConfPara0NameMeta("dummy");
+            ConfPara0NameMeta.erase();
+            ConfPara0NameMeta.append(Surf0Common);
+            ConfPara0NameMeta.append("-confPara0.meta");
+            itkConverter->WriteMeta( meshSO, ConfPara0NameMeta.c_str() );
 
             typedef itk::SpatialObjectReader<3, double, OutputMeshType::MeshTraits> ReaderType;
             ReaderType::Pointer readerSH = ReaderType::New();
@@ -206,9 +225,9 @@ int main( int argc, char * argv[] )
             {
                 if( debug )
                 {
-                    std::cout << "Reading Confpara.meta" << std::endl;
+                    std::cout << "Reading  " << ConfPara0NameMeta << std::endl;
                 }
-                readerSH->SetFileName("Confpara.meta");
+                readerSH->SetFileName(ConfPara0NameMeta);
                 readerSH->Update();
                 ReaderType::SceneType::Pointer          scene1 = readerSH->GetScene();
                 ReaderType::SceneType::ObjectListType * objList =  scene1->GetObjects(1, NULL);
@@ -223,7 +242,6 @@ int main( int argc, char * argv[] )
                 std::cout << ex.GetDescription() << std::endl;
                 return EXIT_FAILURE ;
             }
-
             // Use ConformalMapping output as input
             meshsrc->SetInput(image);
             meshsrc->SetNumberOfIterations(numIterations);
@@ -269,7 +287,6 @@ int main( int argc, char * argv[] )
                     }
                 }
 
-                //This VTK to meta converter doesn't work. when init is set to vtk, it doesn't work
                 if (strstr(initParaFileName.c_str(),".vtk"))
                 {
                     // convert surfaces to meta
@@ -281,17 +298,13 @@ int main( int argc, char * argv[] )
                         }
 
                         // Read VTK
-                        vtkSmartPointer<vtkPolyDataReader> reader = vtkSmartPointer<vtkPolyDataReader>::New();
-                        reader->SetFileName( initParaFileName.c_str() );
-                        reader->Update();
-                        vtkSmartPointer<vtkPolyData> polydata = reader->GetOutput();
+                        vtkSmartPointer<vtkPolyData> polydata = ReadPolyData(initParaFileName);
                         //make sure it reads the VTK correctly
                         if( debug )
                         {
                             std::cout << initParaFileName << " has " << polydata->GetNumberOfPoints() << " points." << std::endl;
                         }
                         // convert to itk mesh data structure
-                        vtkPolyDataToitkMesh vtkItkConverter;
                         vtkItkConverter.SetInput( polydata );
 
                         if (debug) {
@@ -302,11 +315,17 @@ int main( int argc, char * argv[] )
                         itkMeshSOType::Pointer meshSO = itkMeshSOType::New();
                         meshSO->SetMesh( vtkItkConverter.GetOutput() );
                         MeshConverterType::Pointer itkConverter = MeshConverterType::New();
-                        itkConverter->WriteMeta( meshSO, "InitPara.meta" );
+
+                        std::string ConfPara0NameMetaPrefix = generatePrefix("Init", infile, initParaFileName, numIterations);
+
+                        std::string ConfPara0NameMeta("dummy");
+                        ConfPara0NameMeta.erase();
+                        ConfPara0NameMeta.append(ConfPara0NameMetaPrefix);
+                        ConfPara0NameMeta.append("-confPara0.meta");
+                        itkConverter->WriteMeta( meshSO, ConfPara0NameMeta.c_str() );
 
 
                         typedef itk::SpatialObjectReader<3, double, OutputMeshType::MeshTraits> ReaderType;
-                        ReaderType::Pointer readerSH = ReaderType::New();
                         try
                         {
                             if( debug )
@@ -455,4 +474,46 @@ vtkSmartPointer<vtkPolyData> ReadPolyData(std::string filePath)
         return VTKreader->GetOutput();
     }
     return NULL;
+}
+
+void WritePolyData(MeshType::Pointer meshInput, std::string &fileName)
+{
+    ITKWriterType::Pointer ITKwriter = ITKWriterType::New();
+    ITKwriter->SetInput(meshInput);
+    ITKwriter->SetFileName(fileName);
+    ITKwriter->Update();
+}
+//generate prefix of filename as "label(string before first dot of the original filename)-iter[numIterations]"
+std::string generatePrefix(std::string ConfOrInit, std::string infile, std::string initParaFileName, int numIterations)
+{
+    std::string prefix("dummy");
+    prefix.erase();
+    std::istringstream iss;
+    if (ConfOrInit.compare("ConfInit"))
+    {
+        iss.str(infile);
+    }
+    else if (ConfOrInit.compare("Init"))
+    {
+        iss.str(initParaFileName);
+    }
+    else
+    {
+        return NULL;
+    }
+    std::vector<std::string> tokens;
+    std::string token;
+    while (std::getline(iss, token, '.'))
+    {
+        if (!token.empty())
+            tokens.push_back(token);
+    }
+    prefix.append(tokens[0]);
+    prefix.append("-iter");
+    std::stringstream ni;
+    ni << numIterations;
+    string iter = ni.str();
+    prefix.append(iter);
+//    std::cout<<"prefix generated is: " << prefix <<endl;
+    return prefix;
 }
